40a9c3fcb61e407d4bbbb483b7562654d80946e9
Guilherme Simao <guilherme.1301703@fapi-pinhais.edu.br>
2018-09-14 20:20:32 -0300

    Login feito, fazendo cadastro pessoas

--- ProvaGuilherme/Pages/Index.cshtml.cs
+++ ProvaGuilherme/Pages/Index.cshtml.cs
@@ -9,9 +9,22 @@ namespace ProvaGuilherme.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        public string Login { get; set; }
+        [BindProperty]
+        public string Password { get; set; }
+
         public void OnGet()
         {
 
+        }
+
+        public RedirectToPageResult OnPost()
+        {
+            if (this.Login == "guilherme.1301703@fapi-pinhais.edu.br" && this.Password == "1301703")
+                return RedirectToPage("Menu");
+            else
+                return null;
         }
     }
 }
