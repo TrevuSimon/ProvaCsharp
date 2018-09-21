f88c9dbb04fe8b47ab720f3b7e96a0ca4e388681
Guilherme Simao <guilherme.1301703@fapi-pinhais.edu.br>
2018-09-21 19:33:28 -0300

    Finalizado falta testar

--- ProvaGuilherme/Pages/CadastroDetalhes.cshtml.cs
+++ ProvaGuilherme/Pages/CadastroDetalhes.cshtml.cs
@@ -9,9 +9,25 @@ namespace ProvaGuilherme.Pages
 {
     public class CadastroDetalhesModel : PageModel
     {
-        public void OnGet()
-        {
+        public CadastroDetalhesModel Result { get; set; }
 
+        [BindProperty]
+        public String Comida { get; set; }
+        [BindProperty]
+        public String Cor { get; set; }
+        [BindProperty]
+        public String Suco { get; set; }
+        [BindProperty]
+        public String Restaurante { get; set; }
+
+        public void OnGet(object obj)
+        {
+            Result = (CadastroDetalhesModel)obj;
+        }
+
+        public RedirectToPageResult OnPost()
+        {
+            return RedirectToPage("CadastroDetalhes", this); ;
         }
     }
 }
